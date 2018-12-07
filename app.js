06d4c3b4801eecfc87c376ac415941e2c3bc47f1
Sergey Pogorelov <sergey.pogorelov.93@gmail.com>
2018-12-07 20:17:44 +0400

    Add required modules and config

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-import http from 'http';
+import * as config from './config/config.dev.json';
+import * as models from './models';
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:1337/');
+console.log(config.name);
+new models.Product();
+new models.User();
