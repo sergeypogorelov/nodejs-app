47049d8b6d7655fc4089f583f2a3191938a91068
Sergey Pogorelov <sergey.pogorelov.93@gmail.com>
2019-03-20 21:48:47 +0400

    Implement passport local auth

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
+import passport from 'passport';
+import * as passportLocal from 'passport-local';
+import expressSession from 'express-session';
+import * as connectEnsureLogin from 'connect-ensure-login';
+
 import * as middlewares from './middlewares';
 import * as routes from './routes';
+
+passport.use(new passportLocal.Strategy(
+    {
+        usernameField: 'username',
+        passwordField: 'password'
+    },
+    (username, password, cb) => {
+        cb(null, { username });
+    }
+));
+
+passport.serializeUser((user, cb) => {
+    cb(null, user.username);
+});
+  
+passport.deserializeUser((username, cb) => {
+    cb(null, { username })
+});
 
 const app = express();
 
@@ -10,9 +33,15 @@ app.use(cookieParser());
 app.use(middlewares.cookieParserMiddleware());
 app.use(middlewares.queryParserMiddleware());
 
+app.use(expressSession({ secret: 'shh...', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/passport', passport.authenticate('local'), (req, res) => res.send('OK'));
+
 app.use('/auth', routes.authRouter);
-app.use('/api/products', middlewares.tokenCheckerMiddleware(), routes.productsRouter);
-app.use('/api/users', middlewares.tokenCheckerMiddleware(), routes.usersRouter);
+app.use('/api/products', connectEnsureLogin.ensureLoggedIn(), routes.productsRouter);
+app.use('/api/users', connectEnsureLogin.ensureLoggedIn(), routes.usersRouter);
 
 // app.get('/test-middlewares', (req, res, next) => {
 //     const { parsedCookies, parsedQuery } = req;
@@ -20,6 +49,6 @@ app.use('/api/users', middlewares.tokenCheckerMiddleware(), routes.usersRouter);
 //     next();
 // });
 
-app.use(middlewares.errorHandlerMiddleware());
+//app.use(middlewares.errorHandlerMiddleware());
 
 export default app;
